ea3986cc90816c504075441c28ac0fc6684e5c3c
Ken <ken@Kens-MacBook-Air-2.local>
2025-10-08 21:58:24 +0100

    Start

---
+++ MelS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MelS
+//
+//  Created by Ken on 08/10/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
